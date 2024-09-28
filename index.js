20b7a60a8190efc0de47f2763b0a524da79557b6
Yash Panjwani <yashmpanjwani@gmail.com>
2024-09-28 10:33:07 +0530

    yash hello

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 app.use(express.json());
-app.use(cors({
-  origin: "*",
-  
-}));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Explicitly set your frontend origin
+    credentials: true, // Allow credentials
+  })
+);
 
 const proxyURL = "https://www.indiawaterportal.org";
 
@@ -16,6 +19,7 @@ app.use(
   createProxyMiddleware({
     target: proxyURL,
     changeOrigin: true,
+    secure: false, // This can be useful if you're dealing with SSL issues
   })
 );
 
